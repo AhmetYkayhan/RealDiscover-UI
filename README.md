902c2438c3221c459402fcf5171bfd094394bb83
A. Yasin Kayhan <kayhanYasinAhmet@gmail.com>
2022-01-12 14:50:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RealDiscover-UI
+ 
---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealDiscover-UI
+//
+//  Created by Ahmet Yasin Kayhan on 12.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
