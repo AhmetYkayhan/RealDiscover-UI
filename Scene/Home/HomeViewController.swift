17529cf8f9abd25370dd26301ca2815fcca632f4
A. Yasin Kayhan <kayhanYasinAhmet@gmail.com>
2022-01-20 11:51:58 +0300

    add
    UI of sign done
    UI of home and four more page created

---
+++ Scene/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  RealDiscover-UI
+//
+//  Created by Ahmet Yasin Kayhan on 17.01.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+  
+    
+    
+}
